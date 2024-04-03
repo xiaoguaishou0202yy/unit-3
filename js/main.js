9491f00f56db7c2ee5337eba1cf22315b94e1b89
xiaoguaishou0202yy <xiaoguaishou0202yy@gmail.com>
2024-04-02 19:02:31 -0500

    Update

--- js/main.js
+++ js/main.js
@@ -3,21 +3,70 @@
 //begin script when window loads
 window.onload = setMap();
 
-//set up choropleth map
+//Example 1.3 line 4...set up choropleth map
 function setMap(){
-    //use Promise.all to parallelize asynchronous data loading
-    var promises = [d3.csv("data/violent_crime_rate3.csv"),                    
-                    d3.json("data/EuropeCountries.geojson"),                    
-                    d3.json("data/FranceRegions.geojson")                   
-                    ];    
-        Promise.all(promises).then(callback);    
 
-        function callback(data){    
-            csvData = data[0];    
-            europe = data[1];    
-            france = data[2];
-            console.log(csvData);
-            console.log(europe);
-            console.log(france);    
-        };
-};
+    //map frame dimensions
+    var width = 960,
+        height = 460;
+
+    //create new svg container for the map
+    var map = d3.select("body")
+        .append("svg")
+        .attr("class", "map")
+        .attr("width", width)
+        .attr("height", height);
+
+    //create Albers equal area conic projection centered on France
+    var projection = d3.geoAlbers()
+        .center([0, 46.2])
+        .rotate([-2, 0, 0])
+        .parallels([43, 62])
+        .scale(2500)
+        .translate([width / 2, height / 2]);
+
+    var path = d3.geoPath()
+        .projection(projection);
+
+    //use Promise.all to parallelize asynchronous data loading
+    var promises = [];    
+    promises.push(d3.csv("data/violent_crime_rate1.csv")); //load attributes from csv    
+    //promises.push(d3.json("data/EuropeCountries.topojson")); //load background spatial data    
+    promises.push(d3.json("data/states.topojson")); //load choropleth spatial data    
+    Promise.all(promises).then(callback);
+
+    function callback(data){    
+        csvData = data[0];    
+        states = data[1];   
+        //france = data[2]; 
+
+        console.log(csvData);
+        console.log(states);
+
+        // Translate TopoJSON to GeoJSON
+        var usStates = topojson.feature(states, states.objects.ne_110m_admin_1_states_provinces);
+            //franceRegions = topojson.feature(france, france.objects.FranceRegions).features;
+
+        console.log(usStates);
+
+        //add Europe countries to map
+        var regions = map.append("path")
+            .datum(usStates)
+            .attr("class", "countries")
+            .attr("d", path);
+
+        //add France regions to map
+        var regions = map.selectAll(".regions")
+            .data(franceRegions)
+            .enter()
+            .append("path")
+            .attr("class", function(d){
+                return "regions " + d.properties.adm1_code;
+            })
+            .attr("d", path);
+    
+    };
+
+}
+ 
+
