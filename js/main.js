e7a4a7d3ebd5d7c3c26ca35eb6366c9aef6fbebe
xiaoguaishou0202yy <xiaoguaishou0202yy@gmail.com>
2024-04-03 17:53:53 -0500

    Activity 9

--- js/main.js
+++ js/main.js
@@ -7,8 +7,8 @@ window.onload = setMap();
 function setMap(){
 
     //map frame dimensions
-    var width = 960,
-        height = 460;
+    var width = 1000,
+        height = 680;
 
     //create new svg container for the map
     var map = d3.select("body")
@@ -19,10 +19,10 @@ function setMap(){
 
     //create Albers equal area conic projection centered on France
     var projection = d3.geoAlbers()
-        .center([0, 46.2])
-        .rotate([-2, 0, 0])
-        .parallels([43, 62])
-        .scale(2500)
+        .center([3.64, 50])
+        .rotate([102, 0, 0])
+        .parallels([40, 75])
+        .scale(630)
         .translate([width / 2, height / 2]);
 
     var path = d3.geoPath()
@@ -31,39 +31,62 @@ function setMap(){
     //use Promise.all to parallelize asynchronous data loading
     var promises = [];    
     promises.push(d3.csv("data/violent_crime_rate1.csv")); //load attributes from csv    
-    //promises.push(d3.json("data/EuropeCountries.topojson")); //load background spatial data    
+    promises.push(d3.json("data/countries.topojson")); //load background spatial data    
     promises.push(d3.json("data/states.topojson")); //load choropleth spatial data    
     Promise.all(promises).then(callback);
 
     function callback(data){    
         csvData = data[0];    
-        states = data[1];   
-        //france = data[2]; 
+        countries = data[1];   
+        states = data[2]; 
 
         console.log(csvData);
+        console.log(countries.objects);
         console.log(states);
 
         // Translate TopoJSON to GeoJSON
-        var usStates = topojson.feature(states, states.objects.ne_110m_admin_1_states_provinces);
-            //franceRegions = topojson.feature(france, france.objects.FranceRegions).features;
+        var worldCountries = topojson.feature(countries, countries.objects.world_administrative_boundaries),
+            usStates = topojson.feature(states, states.objects.ne_110m_admin_1_states_provinces).features;
 
+        console.log(worldCountries);
         console.log(usStates);
 
+
+        //create graticule generator
+        var graticule = d3.geoGraticule()
+            .step([10, 10]); //place graticule lines every 5 degrees of longitude and latitude
+
+        //create graticule background
+        var gratBackground = map.append("path")
+            .datum(graticule.outline()) //bind graticule background
+            .attr("class", "gratBackground") //assign class for styling
+            .attr("d", path) //project graticule
+
+        //create graticule lines
+        var gratLines = map.selectAll(".gratLines") //select graticule elements that will be created
+            .data(graticule.lines()) //bind graticule lines to each element to be created
+            .enter() //create an element for each datum
+            .append("path") //append each element to the svg as a path element
+            .attr("class", "gratLines") //assign class for styling
+            .attr("d", path); //project graticule lines
+            
         //add Europe countries to map
-        var regions = map.append("path")
-            .datum(usStates)
+        var country = map.append("path")
+            .datum(worldCountries)
             .attr("class", "countries")
             .attr("d", path);
 
         //add France regions to map
-        var regions = map.selectAll(".regions")
-            .data(franceRegions)
+        var state = map.selectAll(".regions")
+            .data(usStates)
             .enter()
             .append("path")
             .attr("class", function(d){
                 return "regions " + d.properties.adm1_code;
             })
             .attr("d", path);
+        
+        console.log(state);
     
     };
 
