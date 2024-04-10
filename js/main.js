e6b28f56a92c2f9f283c473a51e42d6bbbc7eb1d
xiaoguaishou0202yy@gmail.com <xiaoguaishou0202yy@gmail.com>
2024-04-09 21:48:14 -0500

    Update

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 // Add all scripts to the JS folder
 
+var attrArray = ["Violent Crime", "Homicide", "Rape", "Robbery", "Ag. Assault", "Unemployment Rate (%)"];
+var expressed = attrArray[0]; //initial attribute
+
 //begin script when window loads
 window.onload = setMap();
 
@@ -7,7 +10,7 @@ window.onload = setMap();
 function setMap(){
 
     //map frame dimensions
-    var width = 1000,
+    var width = window.innerWidth * 0.5,
         height = 680;
 
     //create new svg container for the map
@@ -40,35 +43,24 @@ function setMap(){
         countries = data[1];   
         states = data[2]; 
 
-        console.log(csvData);
-        console.log(countries.objects);
-        console.log(states);
+        //console.log(csvData);
+        //console.log(countries.objects);
+        //console.log(states);
+
+        //place graticule on the map
+        setGraticule(map, path);
 
         // Translate TopoJSON to GeoJSON
         var worldCountries = topojson.feature(countries, countries.objects.world_administrative_boundaries),
             usStates = topojson.feature(states, states.objects.ne_110m_admin_1_states_provinces).features;
 
         console.log(worldCountries);
+        //console.log(usStates);
+       
+        //join csv data to GeoJSON enumeration units
+        usStates = joinData(usStates, csvData);
+
         console.log(usStates);
-
-
-        //create graticule generator
-        var graticule = d3.geoGraticule()
-            .step([10, 10]); //place graticule lines every 5 degrees of longitude and latitude
-
-        //create graticule background
-        var gratBackground = map.append("path")
-            .datum(graticule.outline()) //bind graticule background
-            .attr("class", "gratBackground") //assign class for styling
-            .attr("d", path) //project graticule
-
-        //create graticule lines
-        var gratLines = map.selectAll(".gratLines") //select graticule elements that will be created
-            .data(graticule.lines()) //bind graticule lines to each element to be created
-            .enter() //create an element for each datum
-            .append("path") //append each element to the svg as a path element
-            .attr("class", "gratLines") //assign class for styling
-            .attr("d", path); //project graticule lines
             
         //add Europe countries to map
         var country = map.append("path")
@@ -76,20 +68,202 @@ function setMap(){
             .attr("class", "countries")
             .attr("d", path);
 
-        //add France regions to map
-        var state = map.selectAll(".regions")
-            .data(usStates)
-            .enter()
-            .append("path")
-            .attr("class", function(d){
-                return "regions " + d.properties.adm1_code;
-            })
-            .attr("d", path);
+        //create the color scale
+        var colorScale = makeColorScale(csvData);
         
-        console.log(state);
+        //add enumeration units to the map
+        setEnumerationUnits(usStates, map, path, colorScale);
+
+        //add coordinated visualization to the map
+        setChart(csvData, colorScale);
     
     };
+};
 
-}
+function setGraticule(map, path){
+    //create graticule generator
+    var graticule = d3.geoGraticule()
+        .step([10, 10]); //place graticule lines every 5 degrees of longitude and latitude
+
+    //create graticule background
+    var gratBackground = map.append("path")
+        .datum(graticule.outline()) //bind graticule background
+        .attr("class", "gratBackground") //assign class for styling
+        .attr("d", path) //project graticule
+
+    //create graticule lines
+    var gratLines = map.selectAll(".gratLines") //select graticule elements that will be created
+        .data(graticule.lines()) //bind graticule lines to each element to be created
+        .enter() //create an element for each datum
+        .append("path") //append each element to the svg as a path element
+        .attr("class", "gratLines") //assign class for styling
+        .attr("d", path); //project graticule lines
+};
+
+function joinData(usStates, csvData){
+    //loop through csv to assign each set of csv attribute values to geojson region
+    for (var i=0; i<csvData.length; i++){
+        var csvRegion = csvData[i]; //the current region
+        var csvKey = csvRegion.adm1_code; //the CSV primary key
+
+        //loop through geojson regions to find correct region
+        for (var a=0; a<usStates.length; a++){
+
+            var geojsonProps = usStates[a].properties; //the current region geojson properties
+            var geojsonKey = geojsonProps.adm1_code; //the geojson primary key
+
+            //where primary keys match, transfer csv data to geojson properties object
+            if (geojsonKey == csvKey){
+
+                //assign all attributes and values
+                attrArray.forEach(function(attr){
+                    var val = parseFloat(csvRegion[attr]); //get csv attribute value
+                    geojsonProps[attr] = val; //assign attribute and value to geojson properties
+                });
+            };
+        };
+    };
+    return usStates;
+};
+
+function setEnumerationUnits(usStates, map, path, colorScale){
+    //add France regions to map
+    var state = map.selectAll(".regions")
+        .data(usStates)
+        .enter()
+        .append("path")
+        .attr("class", function(d){
+            return "regions " + d.properties.adm1_code;
+        })
+        .attr("d", path)        
+            .style("fill", function(d){            
+                var value = d.properties[expressed];            
+                if(value) {                
+                    return colorScale(d.properties[expressed]);            
+                } else {                
+                    return "#ccc";            
+                }    
+            });
+
+    console.log(state);
+};
  
+//function to create color scale generator
+function makeColorScale(data){
+    var colorClasses = [
+        "#D4B9DA",
+        "#C994C7",
+        "#DF65B0",
+        "#DD1C77",
+        "#980043"
+    ];
 
+    //create color scale generator
+    var colorScale = d3.scaleQuantile()
+        .range(colorClasses);
+
+    //build array of all values of the expressed attribute
+    var domainArray = [];
+    for (var i=0; i<data.length; i++){
+        var val = parseFloat(data[i][expressed]);
+        domainArray.push(val);
+    };
+
+    //assign array of expressed values as scale domain
+    colorScale.domain(domainArray);
+
+    return colorScale;
+};
+
+//function to create coordinated bar chart
+function setChart(csvData, colorScale){
+
+    /*
+    //chart frame dimensions
+    var chartWidth = window.innerWidth * 0.425,
+    chartHeight = 473,
+    leftPadding = 25,
+    rightPadding = 2,
+    topBottomPadding = 5,
+    chartInnerWidth = chartWidth - leftPadding - rightPadding,
+    chartInnerHeight = chartHeight - topBottomPadding * 2,
+    translate = "translate(" + leftPadding + "," + topBottomPadding + ")";
+    */
+    
+    //chart frame dimensions
+    var chartWidth = window.innerWidth * 0.425,
+        chartHeight = 680;
+
+    // Positioning variables
+    var chartRight = 20,
+    chartTop = 8;
+
+    //create a second svg element to hold the bar chart
+    var chart = d3.select("body")
+        .append("svg")
+        .attr("width", chartWidth)
+        .attr("height", chartHeight)
+        .attr("class", "chart")
+        .style("position", "absolute")
+        .style("right", chartRight + "px")
+        .style("top", chartTop + "px");
+    
+    //create a scale to size bars proportionally to frame
+    var yScale = d3.scaleLinear()
+        .range([0, chartHeight])
+        .domain([80, 880]);
+
+    //set bars for each state
+    var bars = chart.selectAll(".bars")
+        .data(csvData)
+        .enter()
+        .append("rect")
+        .sort(function(a, b){
+            return a[expressed]-b[expressed]
+        })
+        .attr("class", function(d){
+            return "bars " + d.adm1_code;
+        })
+        .attr("width", chartWidth / csvData.length - 1)
+        .attr("x", function(d, i){
+            return i * (chartWidth / csvData.length);
+        })
+        .attr("height", function(d){
+            return yScale(parseFloat(d[expressed]));
+        })
+        .attr("y", function(d){
+            return chartHeight - yScale(parseFloat(d[expressed]));
+        })
+        .style("fill", function(d){
+            return colorScale(d[expressed]);
+        });
+
+    //annotate bars with attribute value text
+    var numbers = chart.selectAll(".numbers")
+        .data(csvData)
+        .enter()
+        .append("text")
+        .sort(function(a, b){
+            return a[expressed]-b[expressed]
+        })
+        .attr("class", function(d){
+            return "numbers " + d.adm1_code;
+        })
+        .attr("text-anchor", "middle")
+        .attr("x", function(d, i){
+            var fraction = chartWidth / csvData.length;
+            return i * fraction + (fraction - 1) / 2;
+        })
+        .attr("y", function(d){
+            return chartHeight - yScale(parseFloat(d[expressed])) + 15;
+        })
+        .text(function(d){
+            return d[expressed];
+        });
+    
+    var chartTitle = chart.append("text")
+        .attr("x", 20)
+        .attr("y", 40)
+        .attr("class", "chartTitle")
+        .text("Number of " + expressed + " in each region");
+};
